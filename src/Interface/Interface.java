5a936e6815262c20e8692e3a7b66c3ca2174ae19
rbkasprzyk0 <rbkasprzyk0@LH-83602.frostburg.edu>
2013-09-26 12:56:41 -0400

    - Added  base classes

---
+++ src/Interface/Interface.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interface;
+
+/**
+ *
+ * @author rbkasprzyk0
+ */
+public class Interface {
+    
+}
